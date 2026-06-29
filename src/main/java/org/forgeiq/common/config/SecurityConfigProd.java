6e6968fb22cb634906cf9813fea197f0e52f4aae
Aniket Kumar <aniket07sudo>
2026-06-29 23:30:06 +0530

    Updated frontend url

--- src/main/java/org/forgeiq/common/config/SecurityConfigProd.java
+++ src/main/java/org/forgeiq/common/config/SecurityConfigProd.java
@@ -57,7 +57,7 @@ public class SecurityConfigProd {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://192.168.1.34:5173",
-                "https://forge-iq-seven.vercel.app/",
+                "https://forge-iq-one.vercel.app/",
                 "https://forgeiq-api-production.up.railway.app"
         ));
 
