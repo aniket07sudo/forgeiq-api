576b32f40d198731d1743388f643c9685ac876bd
Aniket Kumar <aniket07sudo>
2026-08-02 20:17:54 +0530

    Updated Config

--- src/main/java/org/forgeiq/common/config/SecurityConfigProd.java
+++ src/main/java/org/forgeiq/common/config/SecurityConfigProd.java
@@ -58,7 +58,7 @@ public class SecurityConfigProd {
                 "http://localhost:4173",
                 "http://localhost:5173",
                 "http://192.168.1.34:5173",
-                "https://forge-iq-one.vercel.app/",
+                "https://forge-iq-one.vercel.app",
                 "https://forgeiq-api-production.up.railway.app"
         ));
 
